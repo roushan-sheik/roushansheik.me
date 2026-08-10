c33fad2ffc9ab8b19e612cafb8f89d8c0afcae18
roushan-sheik <roushansheik@gmail.com>
2026-08-10 19:57:49 +0600

    refactor: remove RSS link and icon from footer

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { socialLinks } from "@/data/socialLinks";
-import { FaRss } from "react-icons/fa6";
 import { GoMail } from "react-icons/go";
 
 const Footer = () => {
@@ -7,7 +6,7 @@ const Footer = () => {
     <footer>
       {/* footer parent div  */}
       <div className="flex flex-col gap-4 mt-20 mb-12 items-center justify-center">
-        {/* top row: Subscribe & RSS */}
+        {/* top row: Subscribe */}
         <div className="flex gap-6 items-center">
           <a
             href="#"
@@ -15,13 +14,6 @@ const Footer = () => {
           >
             <GoMail className="text-lg" />
             <p className="text-sm font-medium">Subscribe</p>
-          </a>
-          <a
-            href="#"
-            className="flex hover:text-gray-600 duration-300 ease-in gap-2 items-center cursor-pointer text-black"
-          >
-            <FaRss className="text-lg" />
-            <p className="text-sm font-medium">RSS</p>
           </a>
         </div>
         {/* bottom row: Social Links */}
